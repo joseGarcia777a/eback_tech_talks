0a793818bacc6d1dfbfb0fe30ae59809b036eb8b
Jose Garcia de Souza Junior <jose_garcia2022@outlook.com.br>
2024-07-09 11:11:42 -0300

    criando a contagem regreciva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 AOS.init();
+
+const dataEvento = new Date("aug 12, 2024 19:00:00");
+const timeStampDoEvento = dataEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteEvento = Math.floor(distanciaAteEvento / diaEmMs);
+    const horasAteEvento = Math.floor((distanciaAteEvento % diaEmMs) / horaEmMs);
+    const minutosAteEvento = Math.floor((distanciaAteEvento % horaEmMs) / minutoEmMs);
+    const segundosAteEvento = Math.floor((distanciaAteEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteEvento}d ${horasAteEvento}h ${minutosAteEvento}m ${segundosAteEvento}s`;
+
+    if (distanciaAteEvento < 0) {
+        clearInterval(contaAsHoras);
+        ocument.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
